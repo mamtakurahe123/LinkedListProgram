65906f4f6e90bf341e342709b3a36db84b3c4598
mamta maskare <mamta maskare>
2022-03-19 15:24:52 +0530

    Linked List Program has been commted

---
+++ LinkedListExercise7.java
@@ -0,0 +1,21 @@
+package linkedlistprogram;
+
+import java.util.LinkedList;
+
+
+public class LinkedListExercise7 {
+	
+	public static void main(String[] args) {
+		   // create an empty linked list
+		     LinkedList<String> l_list = new LinkedList<String>();
+		   // use add() method to add values in the linked list
+		          l_list.add("Red");
+		          l_list.add("Green");
+		          l_list.add("Black");
+		     System.out.println("Original linked list:" + l_list);    
+		  // Add an element to front of LinkedList
+		     l_list.offerFirst("Pink");
+		     System.out.println("Final linked list:" + l_list);  
+		 }	
+
+}
